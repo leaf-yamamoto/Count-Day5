c5587129df3451f6776019ae8fd88d5e35f3fafb
leaf-yamamoto <sanaeyamamoto@icloud.com>
2021-05-23 11:05:21 +0900

    プラスボタンの完成

---
+++ app/src/main/java/app/yamamoto/leaf/countday5/MainActivity.kt
@@ -0,0 +1,21 @@
+package app.yamamoto.leaf.countday5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number= 0
+
+        plusButton.setOnClickListener{
+            number = number +1
+            numberTextView.text = number.toString()
+        }
+
+
+    }
+}
